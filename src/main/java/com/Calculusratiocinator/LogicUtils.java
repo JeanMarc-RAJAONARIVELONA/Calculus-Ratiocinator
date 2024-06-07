a1cd6324bd103bb17c4348adb28a5c9a8ab39490
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-06-07 11:24:29 +0300

    feat: implement logic utilis in a class

---
+++ src/main/java/com/Calculusratiocinator/LogicUtils.java
@@ -0,0 +1,15 @@
+package com.Calculusratiocinator;
+
+public class LogicUtils {
+    public static boolean AndLogic(boolean a, boolean b) {
+        return a && b;
+    }
+
+    public static boolean OrLogic(boolean a, boolean b) {
+        return a || b;
+    }
+
+    public static boolean NotLogic(boolean a, boolean b) {
+        return !a && b;
+    }
+}
