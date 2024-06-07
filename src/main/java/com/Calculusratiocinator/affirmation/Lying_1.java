f4cf1a2135b05cd02d06cc72300997fe111714f5
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-06-07 11:21:46 +0300

    feat: create Lying class to represente lie affirmation

---
+++ src/main/java/com/Calculusratiocinator/affirmation/Lying_1.java
@@ -0,0 +1,12 @@
+package com.Calculusratiocinator.affirmation;
+
+public final class Lying_1 extends Affirmation {
+    private Lying_1() {
+        super(false);
+    }
+
+    @Override
+    public Boolean isTruth() {
+        return false;
+    }
+}
