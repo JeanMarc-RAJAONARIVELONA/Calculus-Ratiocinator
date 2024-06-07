81335c9fbac0ac14400d57499f8af73eaeea4028
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-06-07 11:22:31 +0300

    feat: create affirmation_1 class to represente lie affirmation not truth or lie

---
+++ src/main/java/com/Calculusratiocinator/affirmation/Affirmation_1.java
@@ -0,0 +1,13 @@
+package com.Calculusratiocinator.affirmation;
+
+public final class Affirmation_1 extends Affirmation {
+
+    private Affirmation_1(Boolean value) {
+        super(value);
+    }
+
+    @Override
+    public Boolean isTruth() {
+        return null;
+    }
+}
